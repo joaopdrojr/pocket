56f51847da0e053681dc7ac8a9fe3fc983f34f08
Joao.pdro <joao.pdrojr@gmail.com>
2025-05-30 16:17:35 -0300

    Initial Commit

---
+++ AppIntentsDemo/AppIntentsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentsDemoApp.swift
+//  AppIntentsDemo
+//
+//  Created by Joao Pedro Junior on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
